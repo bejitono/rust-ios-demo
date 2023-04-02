d32ee5c28e2d949e8a5b7fd40a0592ce3081bd4a
Stefano <stdemicheli@protonmail.com>
2023-04-02 08:12:23 +0200

    Removes redundant files.

--- rust-ios-test-2/rust-ios-test-2/rust-ios-test-2-Bridging-Header.h
+++ rust-ios-test-2/rust-ios-test-2/rust-ios-test-2-Bridging-Header.h
@@ -2,10 +2,10 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#ifndef rust_ios_test_2_Bridging_Header_h
-#define rust_ios_test_2_Bridging_Header_h
+//#ifndef rust_ios_test_2_Bridging_Header_h
+//#define rust_ios_test_2_Bridging_Header_h
 
 #import "todolistFFI.h"
 
-#endif /* rust_ios_test_2_Bridging_Header_h */
+//#endif /* rust_ios_test_2_Bridging_Header_h */
 
